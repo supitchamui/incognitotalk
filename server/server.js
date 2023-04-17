b004df51e0be5ae6b4ffe44cc3fc7101bd75da7f
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2023-04-17 16:43:32 +0700

    feat: connect socket for client

--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const http = require("http");
+const Server = require("socket.io");
+
+dotenv.config();
 
 const app = express();
-app.listen(process.env.PORT, () => {
+app.use(cors);
+const server = http.createServer(app);
+const io = Server(server);
+
+io.on("connection", (socket) => {
+  socket.emit("userId", socket.id);
+  socket.on("send message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+server.listen(process.env.PORT, () => {
   console.log(`Server start on port ${process.env.PORT}`);
 });
