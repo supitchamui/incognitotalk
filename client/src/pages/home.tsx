63dc1594beba6db902d4c96d0ac12984073c97b3
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2023-04-25 01:00:07 +0700

    feat: homepage structure

---
+++ client/src/pages/home.tsx
@@ -0,0 +1,175 @@
+import { useState } from "react";
+import io from "socket.io-client";
+import Image from "next/image";
+
+const URL = process.env.NEXT_PUBLIC_URL ?? "";
+const socket = io(URL, { transports: ["websocket"] });
+
+type Message = {
+  author: string;
+  message: string;
+  time: Date;
+  room: string;
+};
+
+const Home = () => {
+  const [message, setmessage] = useState("");
+  const [id, setId] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [room, setRoom] = useState("");
+  const handleSendMessage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    socket.emit("send-message", {
+      author: id,
+      message: message,
+      time: new Date(),
+      room: room,
+    });
+  };
+
+  const handleJoinRoom = (e: React.MouseEvent<HTMLButtonElement>) => {
+    socket.emit("join-room", { username: id, room: room });
+  };
+
+  socket.on("userId", (id) => {
+    setId(id);
+  });
+  socket.on("message", (data) => {
+    data.time = new Date(data.time);
+    if (messages.length == 0) {
+      setMessages([data]);
+    } else {
+      setMessages([...messages, data]);
+    }
+  });
+
+  return (
+    <>
+      <div className="min-h-screen h-full flex-col flex">
+        <div className="bg-darkBgColor w-full h-20 items-center flex border-b border-borderColor">
+          <div className="flex items-center mr-0">
+            <div className="ml-5">
+              <Image src="/logo1.png" alt="" width={40} height={40}></Image>
+            </div>
+            <p className="ml-5 text-2xl font-lexend-deca text-white">
+              INCOGNITOTALK
+            </p>
+          </div>
+        </div>
+        <div className="flex-1 flex-row flex">
+          <div className="bg-darkBgColor w-28 items-center flex text-white flex-col justify-end font-roboto">
+            <div className="mb-10">
+              <p>Friends</p>
+            </div>
+            <div className="mb-10">
+              <p>Groups</p>
+            </div>
+            <div className="mb-16">
+              <p>All Chats</p>
+            </div>
+          </div>
+          <div className="bg-bgColor w-1/3 border-r border-borderColor">
+            <div className="h-28 w-full border-b border-borderColor items-center flex justify-center">
+              <div className="w-4/5">
+                <input
+                  type="text"
+                  className="w-full h-16 rounded-2xl bg-borderColor pl-5 text-white"
+                  placeholder="Search"
+                  name="search_user"
+                />
+              </div>
+            </div>
+            <div className="h-28 w-full border-b border-borderColor items-center flex cursor-pointer">
+              <Image
+                src="/Frame_8.png"
+                alt=""
+                width={75}
+                height={50}
+                className="ml-6"
+              ></Image>
+              <div className="font-roboto ml-6">
+                <p className="text-white text-xl">Someone</p>
+                <p className="text-fontBgColor text-base mt-2">Past Message</p>
+              </div>
+            </div>
+            <div className="h-28 w-full border-b border-borderColor items-center flex cursor-pointer">
+              <Image
+                src="/Frame_8.png"
+                alt=""
+                width={75}
+                height={50}
+                className="ml-6"
+              ></Image>
+              <div className="font-roboto ml-6">
+                <p className="text-white text-xl">Someone</p>
+                <p className="text-fontBgColor text-base mt-2">Past Message</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-bgColor w-2/3">
+            <div className="w-full h-full justify-center items-center flex-col flex">
+              <Image
+                src="/logo1.png"
+                alt=""
+                width={200}
+                height={200}
+                className="opacity-50"
+              ></Image>
+              <p className="text-xl font-roboto text-white opacity-40 mt-2">
+                Start your new chat!
+              </p>
+            </div>
+            {/*<div className="h-28 w-full border-b border-borderColor flex items-center">*/}
+            {/*    <div className="flex items-center mr-10 w-full justify-between">*/}
+            {/*        <div><p className="ml-10 text-3xl font-roboto text-white font-medium">Someone</p></div>*/}
+            {/*        <div className="w-2/6">*/}
+            {/*            <input type="text" className="w-full h-12 rounded-2xl bg-borderColor pl-5 text-white" placeholder="Search in this chat" name="search_user"/>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div></div>*/}
+            {/*    <div></div>*/}
+            {/*</div>*/}
+          </div>
+        </div>
+      </div>
+    </>
+    // <div className="h-full w-full pt-10 px-5">
+    //   <input
+    //     className="p-2 mr-4 rounded-xl bg-gray-200 border-2 hover:border-black"
+    //     type="text"
+    //     placeholder="room name"
+    //     onChange={(e) => setRoom(e.target.value)}
+    //   />
+    //   <button
+    //     className="p-2 mr-10 rounded-xl bg-blue-300 text-blue-800"
+    //     onClick={handleJoinRoom}
+    //   >
+    //     Join room
+    //   </button>
+    //   <input
+    //     className="p-2 mr-4 rounded-xl bg-gray-200 border-2 hover:border-black"
+    //     type="text"
+    //     placeholder="message"
+    //     onChange={(e) => setmessage(e.target.value)}
+    //   />
+    //   <button
+    //     className="p-2 rounded-xl bg-blue-300 text-blue-800"
+    //     onClick={handleSendMessage}
+    //   >
+    //     Send
+    //   </button>
+    //   <p className="font-bold">
+    //     ID: <span className="font-normal">{id}</span>
+    //   </p>
+    //   <h1 className="font-bold">Messages</h1>
+    //   <div className="pl-10">
+    //     {messages.map((m, index) => (
+    //       <li key={index}>
+    //         {m.author} {m.message} {formatTime(m.time)}
+    //       </li>
+    //     ))}
+    //   </div>
+    // </div>
+  );
+};
+
+export default Home;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,86 +1,9 @@
-import { formatTime } from "@/utils/date";
-import { useState } from "react";
-import io from "socket.io-client";
+import Login from "@/pages/login";
 
-const URL = process.env.NEXT_PUBLIC_URL ?? "";
-const socket = io(URL, { transports: ["websocket"] });
-
-type Message = {
-  author: string;
-  message: string;
-  time: Date;
-  room: string;
-};
-
-const Home = () => {
-  const [message, setmessage] = useState("");
-  const [id, setId] = useState("");
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [room, setRoom] = useState("");
-  const handleSendMessage = (e: React.MouseEvent<HTMLButtonElement>) => {
-    socket.emit("send-message", {
-      author: id,
-      message: message,
-      time: new Date(),
-      room: room,
-    });
-  };
-
-  const handleJoinRoom = (e: React.MouseEvent<HTMLButtonElement>) => {
-    socket.emit("join-room", { username: id, room: room });
-  };
-
-  socket.on("userId", (id) => {
-    setId(id);
-  });
-  socket.on("message", (data) => {
-    data.time = new Date(data.time);
-    if (messages.length == 0) {
-      setMessages([data]);
-    } else {
-      setMessages([...messages, data]);
-    }
-  });
-
+export default function main() {
   return (
-    <div className="h-full w-full pt-10 px-5">
-      <input
-        className="p-2 mr-4 rounded-xl bg-gray-200 border-2 hover:border-black"
-        type="text"
-        placeholder="room name"
-        onChange={(e) => setRoom(e.target.value)}
-      />
-      <button
-        className="p-2 mr-10 rounded-xl bg-blue-300 text-blue-800"
-        onClick={handleJoinRoom}
-      >
-        Join room
-      </button>
-      <input
-        className="p-2 mr-4 rounded-xl bg-gray-200 border-2 hover:border-black"
-        type="text"
-        placeholder="message"
-        onChange={(e) => setmessage(e.target.value)}
-      />
-      <button
-        className="p-2 rounded-xl bg-blue-300 text-blue-800"
-        onClick={handleSendMessage}
-      >
-        Send
-      </button>
-      <p className="font-bold">
-        ID: <span className="font-normal">{id}</span>
-      </p>
-      <h1 className="font-bold">Messages</h1>
-      <div className="pl-10">
-        {messages.map((m, index) => (
-          <li key={index}>
-            {m.author} {m.message} {formatTime(m.time)}
-          </li>
-        ))}
-      </div>
-    </div>
+    <>
+      <Login />
+    </>
   );
-};
-
-export default Home;
+}
