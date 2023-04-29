885569ffb7b11b519546e19e369cc4026ec9281b
Nalin <nalinbaipluthong@gmail.com>
2023-04-29 14:28:59 +0700

    add search handler and redesign layout

--- client/src/pages/Left-Sidebar/sidebar-menu.tsx
+++ client/src/pages/Left-Sidebar/sidebar-menu.tsx
@@ -1,25 +1,31 @@
 type SidebarMenuProps = {
   setPage: (page: string) => void;
+  currentPage: string;
 };
 
 import { UserGroupIcon } from "@heroicons/react/24/outline";
 import { UserIcon } from "@heroicons/react/24/outline";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
-const SidebarMenu = ({ setPage }: SidebarMenuProps) => {
+const SidebarMenu = ({ setPage, currentPage }: SidebarMenuProps) => {
+  const [room, setRoom] = useState("");
   return (
     <div className="bg-darkBgColor w-28 items-center flex text-white flex-col justify-end font-roboto">
       <div className="mb-8">
         <button
           type="button"
           name="friends"
-          className="flex flex-col items-center justify-center w-20 h-20 rounded-xl hover:bg-bgColor"
+          className={`flex flex-col items-center justify-center w-20 h-20 rounded-xl hover:bg-bgColor ${
+            room === "friends" ? "text-purple" : ""
+          }`}
           onClick={() => {
             setPage("friends");
+            setRoom("friends");
             console.log("friends");
           }}
         >
-          <UserIcon className="h-8 w- text-fontWhiteDarkBgColor" />
+          <UserIcon className="h-8 w-8" />
           <span className="text-sm font-medium">Friends</span>
         </button>
       </div>
@@ -27,13 +33,16 @@ const SidebarMenu = ({ setPage }: SidebarMenuProps) => {
         <button
           type="button"
           name="groups"
-          className="flex flex-col items-center justify-center w-20 h-20 rounded-xl hover:bg-bgColor"
+          className={`flex flex-col items-center justify-center w-20 h-20 rounded-xl hover:bg-bgColor ${
+            room === "groups" ? "text-purple" : ""
+          }`}
           onClick={() => {
             setPage("groups");
+            setRoom("groups");
             console.log("groups");
           }}
         >
-          <UserGroupIcon className="h-8 w-8 text-fontWhiteDarkBgColor" />
+          <UserGroupIcon className="h-8 w-8" />
           <span className="text-sm font-medium">Groups</span>
         </button>
       </div>
@@ -42,13 +51,16 @@ const SidebarMenu = ({ setPage }: SidebarMenuProps) => {
         <button
           type="button"
           name="all-chats"
-          className="flex flex-col items-center justify-center w-20 h-20 rounded-xl hover:bg-bgColor"
+          className={`flex flex-col items-center justify-center w-20 h-20 rounded-xl hover:bg-bgColor ${
+            room === "all-chats" ? "text-purple" : ""
+          }`}
           onClick={() => {
             setPage("all-chats");
+            setRoom("all-chats");
             console.log("all-chats");
           }}
         >
-          <ChatBubbleOvalLeftEllipsisIcon className="h-8 w- text-fontWhiteDarkBgColor" />
+          <ChatBubbleOvalLeftEllipsisIcon className="h-8 w-8" />
           <span className="text-sm font-medium">All Chats</span>
         </button>
       </div>
